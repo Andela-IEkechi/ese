64006ba0d27bb4ad7e6616590ae819ef8d5d9a85
Ekechi Ikenna <luccy559slim@gmail.com>
2015-09-07 00:11:41 +0100

    design of home page in progress

--- ese/config/routes.rb
+++ ese/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#show'
+
   devise_for :users
-  root 'coming_soon#show'
+  get 'coming_soon/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
