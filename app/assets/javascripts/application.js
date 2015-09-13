d47760038822c50b31d873fe30d025a6146eace3
Ekechi Ikenna <luccy559slim@gmail.com>
2015-09-13 15:52:43 +0100

    disclaimer modal added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,4 +24,5 @@ $( document ).ready(function() {
 	$('.collapsible').collapsible({
 		accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
 	});
+	$('#disclaimer').openModal();
 });
---
+++ app/helpers/disclaimer_helper.rb
@@ -0,0 +1,7 @@
+module DisclaimerHelper
+
+  def supported_browser?(user_agent_string)
+    browser = Browser.new(ua: user_agent_string )
+    !(browser.chrome? || (browser.ie? && browser.version.to_i < 9))
+  end
+end
