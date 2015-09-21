9b1cb5c4a67e46a75ae3ad5cbb63c52fc170d248
Ekechi Ikenna <luccy559slim@gmail.com>
2015-09-21 21:48:54 +0100

    email template styling almost done

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -4,9 +4,11 @@ class NewsletterMailer < ApplicationMailer
     @articles = Article.this_week
     @users = User.all
     @date = Date.today
-    @url  = "https://esewalter.com/article/"
-    # attachments.inline['logo.png'] = File.read(File.join(Rails.root, 'app','assets', 'images', 'logo.png'))
-    # attachments.inline['signature.png'] = File.read(File.join(Rails.root, 'app','assets', 'images', 'signature.png'))
+    @url  = "https://esewalter.com/articles/"
+    attachments.inline['femina-speaks.png'] = File.read(File.join(Rails.root, 'app','assets', 'images', 'femina-speaks.png'))
+    attachments.inline['facebook.png'] = File.read(File.join(Rails.root, 'app','assets', 'images', 'facebook.png'))
+    attachments.inline['google_buzz.png'] = File.read(File.join(Rails.root, 'app','assets', 'images', 'google_buzz.png'))
+    attachments.inline['twitter.png'] = File.read(File.join(Rails.root, 'app','assets', 'images', 'twitter.png'))
     @users.each do |user|
       mail(to: user.email, subject: 'Femina Speaks Newsletter')
     end
