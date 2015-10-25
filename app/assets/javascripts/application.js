11beb00b36fa63218b3fc27d8d51f76933d54d97
Ekechi Ikenna <luccy559slim@gmail.com>
2015-10-25 09:00:52 +0100

    switched materialize version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,9 +31,9 @@ $( document ).ready(function() {
 	}
 	$('.dropdown-button').dropdown();
 	// Show sideNav
-	$('.button-collapse').sideNav('show');
-// Hide sideNav
-	$('.button-collapse').sideNav('hide');
+//	$('.button-collapse').sideNav('show');
+//// Hide sideNav
+//	$('.button-collapse').sideNav('hide');
 	$('.button-collapse').sideNav({
 			edge: 'right', // Choose the horizontal origin
 			closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
